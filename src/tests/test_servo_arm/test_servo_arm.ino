13471b9fdb713d368b62b62a0c438844ba0ce9c9
rataflo <rataflo@freefr>
2020-01-15 17:28:52 +0100

    open & close arm

--- src/tests/test_servo_arm/test_servo_arm.ino
+++ src/tests/test_servo_arm/test_servo_arm.ino
@@ -9,16 +9,14 @@ void setup() {
 // servo arm position
   servoOpen.attach(SERVO_ARM_OPEN);
   servoOpen.write(0);  
-  servoClose.attach(SERVO_ARM_CLOSE);
-  servoClose.write(0);  
 }
 
 void loop() {
   //positions test
   //servoOpen.write(10);
-  servoClose.write(180);
+  //servoClose.write(180);
 
-  for (pos = 10; pos <= 120; pos += 1) { // goes from 0 degrees to 180 degrees
+  /*for (pos = 10; pos <= 120; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     servoOpen.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
@@ -26,10 +24,11 @@ void loop() {
   for (pos = 120; pos >= 10; pos -= 1) { // goes from 180 degrees to 0 degrees
     servoOpen.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
-  }
+  }*/
 
+  servoOpen.write(0);
   // Opern & close degrees:
-  Open: 10=>120
-  Close: 180 => 40?
-  Rest: 180. Nécessite d'être positionné à 10 avant rotation.
+  //Open: 10=>120
+  //Close: 180 => 40?
+  //Rest: 180. Nécessite d'être positionné à 10 avant rotation.
 }
